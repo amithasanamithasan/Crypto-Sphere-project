30d21acd0682d31151695c946b699ee05fde5fb5
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-09-22 00:24:03 +0600

    navbar component using

--- src/Component/Pages/Navbar/Navbar.jsx
+++ src/Component/Pages/Navbar/Navbar.jsx
@@ -2,9 +2,28 @@
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>this is our header section</h1>
-        </div>
+    <div>
+            <nav className="flex justify-between items-center p-6 
+            bg-[#161042] ;">
+    <div className="flex items-center">
+        <image src="logo.png" alt="Crypto Sphere" className="h-10"> </image>
+   <h1 className="ml-3 text-xl font-semibold text-cyan-400 "> Crypto</h1>   <span className="ml-3 text-xl font-semibold text-cyan-200">  Sphere</span>
+      </div>
+    <ul className="flex space-x-6">
+      <li><a href="#" className="text-white hover:text-cyan-500">Home</a></li>
+      <li><a href="#" className="text-white hover:text-cyan-500">Features</a></li>
+      <li><a href="#" className="text-white hover:text-cyan-500">About</a></li>
+      <li><a href="#" className="text-white hover:text-cyan-500">Services</a></li>
+      <li><a href="#" className="text-white hover:text-cyan-500">Testimonials</a></li>
+      <li><a href="#" className="text-white hover:text-cyan-500">Pricing</a></li>
+      <li><a href="#" className="text-white hover:text-cyan-500">FAQ</a></li>
+      <li><a href="#" className="text-white hover:text-cyan-500">Contact</a></li>
+    </ul>
+    <div>
+      <a href="#" className="bg-cyan-500 text-black px-4 py-2 rounded-lg hover:bg-cyan-600">Sign Up</a>
+    </div>
+  </nav>
+  </div>
     );
 };
 
